abc3cee443d7bdc87e306dd8ae9fe6d4d61e019f
Stylon Wang <stylon.wang@avermedia.com>
2014-12-24 18:56:50 +0800

    Mostly-working version, after strugling with VTIME & VMIN.
    	modified:   Makefile
    	modified:   receiver.c
    	modified:   sender.c
    	modified:   uart.c

--- uart/receiver.c
+++ uart/receiver.c
@@ -24,7 +24,7 @@ int fd;
 void signal_handler(int signo)
 {
     fprintf(stderr, "exiting...\n");
-	tcsetattr(fd, TCSANOW, &oldtio);
+    tcsetattr(fd, TCSANOW, &oldtio);
     close(fd);
     exit(0);
 }
@@ -33,7 +33,7 @@ void signal_handler(int signo)
 
 int main(int argc, char **argv)
 {
-    if(argc<2) {
+    if (argc < 2) {
         fprintf(stderr, "Usage: %s tty-device\n", argv[0]);
         exit(1);
     }
@@ -42,18 +42,48 @@ int main(int argc, char **argv)
 
     fd = ubus_uart_init(argv[1], 115200);
 
-    if(fd<0) exit(1);
+    if (fd < 0)
+        exit(1);
 
     fprintf(stderr, "Start receiving\n");
-    while(1) {
-        int i=0;
-
-        ssize_t s = read(fd, &i, sizeof(i));
-        if(s<0) {
+    while (1) {
+        unsigned char c[4];
+        int ret;
+        fd_set readfs;
+        struct timeval timeout;
+        
+		timeout.tv_usec = 0L;
+		timeout.tv_sec = 0; //50L;
+		FD_ZERO(&readfs);
+		FD_SET(fd, &readfs);
+#if 0
+		ret = select(fd + 1, &readfs, NULL, NULL, &timeout);
+        if(ret<0 && errno!=EINTR) {
+            LOG_ERR("select failed: %s\n", strerror(errno));
+            sleep(2);
+            continue;
+		}
+		else if (ret == 0) {
+            fprintf(stderr, "waiting...\n");
+            continue; // timeout with no data
+		}
+#endif
+        // read from RS232
+        ssize_t s = read(fd, &c, sizeof(c));
+        if (s < 0) {
             fprintf(stderr, "error reading: %s\n", strerror(errno));
             exit(1);
-        }  
-        fprintf(stderr, "Got 0x%x\n", i);
+        }
+        else if(s==0) {
+        }
+        else {
+            int i=0;
+            fprintf(stderr, "Got(%d) ", s);
+            for(i=0; i<s; ++i) {
+                fprintf(stderr, "%d ", c[i]);
+            }
+            fprintf(stderr, "\n");
+        }
     }
 
     close(fd);
--- uart/sender.c
+++ uart/sender.c
@@ -24,7 +24,7 @@ int fd;
 void signal_handler(int signo)
 {
     fprintf(stderr, "exiting...\n");
-	tcsetattr(fd, TCSANOW, &oldtio);
+    tcsetattr(fd, TCSANOW, &oldtio);
     close(fd);
     exit(0);
 }
@@ -33,7 +33,7 @@ void signal_handler(int signo)
 
 int main(int argc, char **argv)
 {
-    if(argc<2) {
+    if (argc < 2) {
         fprintf(stderr, "Usage: %s tty-device\n", argv[0]);
         exit(1);
     }
@@ -42,22 +42,24 @@ int main(int argc, char **argv)
 
     fd = ubus_uart_init(argv[1], 115200);
 
-    if(fd<0) exit(1);
+    if (fd < 0)
+        exit(1);
 
     fprintf(stderr, "Halt....\n");
     sleep(1);
-    while(1) {
-        static unsigned char i=0;
-        
-        fprintf(stderr, "Sending 0x%x\n", i);
-        ssize_t s = write(fd, &i, sizeof(i));
-        if(s<0) {
+    while (1) {
+        static unsigned char c[4];
+        static int i=0;
+
+        c[0] = i++; c[1] = i++; c[2] = i++; c[3] = i++;
+        fprintf(stderr, "Sending %d %d %d %d\n", c[0], c[1], c[2], c[3]);
+        ssize_t s = write(fd, c, sizeof(c));
+        if (s < 0) {
             fprintf(stderr, "error writing: %s\n", strerror(errno));
             exit(1);
-        }  
+        }
 
-        usleep(700*1000);
-        i++;
+        usleep(1200 * 1000);
     }
 
     close(fd);
